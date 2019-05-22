b71af2a865a6a431beb9337b7ff319c23ea8a6d8
OlegKoleskin <oleg.koleskin@gmail.com>
2019-05-22 10:00:13 +0300

    update sw.js

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', (event) => {
 	console.log('Service worker activated', event);
 	return self.clients.claim();
 });
+
+self.addEventListener('fetch', (event) => {
+  console.log('serwice worker fetching...', event);
+  event.respondWith(fetch(event.request));
+});
