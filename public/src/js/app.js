d25924994bd83b317d337766fb9bb9fe6acbf31f
OlegKoleskin <oleg.koleskin@gmail.com>
2019-05-24 08:34:46 +0300

    wip

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,12 +1,16 @@
 var deferredPrompt;
 
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(function(){
-		console.log('Service worker registered')
-	});
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-window.addEventListener('beforeinstallprompt', (event) =>{
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function () {
+    console.log('Service worker registered')
+  });
+}
+
+window.addEventListener('beforeinstallprompt', (event) => {
   console.log('beforeinstallprompt fired');
   event.preventDefault();
   deferredPrompt = event;
