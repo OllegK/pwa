5082ff5fdcc51410edde56a3cb1463fa03cff40e
OllegK <oleg.koleskin@gmail.com>
2019-05-25 12:17:25 +0300

    added static caching

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,42 @@
 self.addEventListener('install', (event) => {
-	console.log('Service worker installed', event);
+  console.log('[SW] installed.....................', event);
+  event.waitUntil(
+    caches.open('static')
+      .then((cache) => {
+        console.log('[SW] precaching app shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+
+        ]);
+      }))
 });
 
 self.addEventListener('activate', (event) => {
-	console.log('Service worker activated', event);
+	console.log('[SW] activated', event);
 	return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('serwice worker fetching...', event);
-  event.respondWith(fetch(event.request));
+  console.log('[SW] fetching...', event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
