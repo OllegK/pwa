7357baf7b0562f783b5cccaad81a05b505893e37
OllegK <oleg.koleskin@gmail.com>
2019-06-01 12:01:09 +0300

    updated sw

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,21 @@
-const CACHE_STATIC_NAME = 'static-v10';
-const CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const CACHE_STATIC_NAME = 'static-v13';
+const CACHE_DYNAMIC_NAME = 'dynamic-v5';
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
 
 self.addEventListener('install', (event) => {
   console.log('[SW] installed.....................', event);
@@ -7,23 +23,7 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_STATIC_NAME)
       .then((cache) => {
         console.log('[SW] precaching app shell');
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/offline.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-
-        ]);
+        cache.addAll(STATIC_FILES);
       }))
 });
 
@@ -41,12 +41,17 @@ self.addEventListener('activate', (event) => {
   return self.clients.claim();
 });
 
+const isInArray = (string, array) => {
+  return array.map(el => self.location.origin + el).includes(string);
+}
+
 self.addEventListener('fetch', (event) => {
 
   // console.log('[SW] fetching...', event);
   const url = 'https://httpbin.org/get';
 
   if (event.request.url.indexOf(url) > -1) {
+    console.log('1st', event.request.url);
     event.respondWith(
       caches.open(CACHE_DYNAMIC_NAME)
         .then(cache => {
@@ -56,7 +61,13 @@ self.addEventListener('fetch', (event) => {
           })
         })
     );
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    console.log('2nd', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(response => response)
+    );
   } else {
+    console.log('3rd', event.request.url);
     event.respondWith(
       caches.match(event.request)
         .then(response => {
@@ -71,13 +82,14 @@ self.addEventListener('fetch', (event) => {
             .catch((err) => {
               console.log(err);
               return caches.open(CACHE_STATIC_NAME).then(cache => {
-                return cache.match('/offline.html');
+                if (event.request.url.indexOf('/help') > -1) {
+                  return cache.match('/offline.html');
+                }
               });
             });
         })
     )
   }
-
 });
 
 // self.addEventListener('fetch', (event) => {
